e5c35ca384bdca89f090dec0b84b2d9d35d61191
Daniel <daniel.hpf@hotmail.com>
2022-05-21 18:09:26 +0200

    small changes in data preparation

--- classificador2.py
+++ classificador2.py
@@ -10,16 +10,20 @@ data['pollutant'] = le.fit_transform(data['pollutant'])
 data['countryName'] = le.fit_transform(data['countryName'])
 data['eprtrSectorName'] = le.fit_transform(data['eprtrSectorName'])
 data['City'] = le.fit_transform(data['City'])
+data['EPRTRAnnexIMainActivityCode'] = le.fit_transform(data['EPRTRAnnexIMainActivityCode'])
+
+
+
+
 
 features = data.columns.tolist()
 features.remove('pollutant')
 X = data[features]
 y = data['pollutant']
-print(features)
 
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-clf = RandomForestClassifier(n_estimators=10)
+clf = RandomForestClassifier(n_estimators=200)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 conf_mat = metrics.confusion_matrix(y_test, y_pred)
@@ -31,3 +35,15 @@ print(metrics.f1_score(y_test, y_pred, average='micro'))
 
 feature_imp = pandas.Series(clf.feature_importances_, index=features).sort_values(ascending=False)
 print(feature_imp.to_string())
+
+FP_0 = conf_mat[1][0] + conf_mat[2][0]
+FN_0 = conf_mat[0][1] + conf_mat[0][2]
+FP_1 = conf_mat[0][1] + conf_mat[2][1]
+FN_1 = conf_mat[1][0] + conf_mat[1][2]
+FP_2 = conf_mat[0][2] + conf_mat[1][2]
+FN_2 = conf_mat[2][0] + conf_mat[2][1]
+
+FP = FP_0 + FP_1 + FP_2
+FN = FN_0 + FN_1 + FN_2
+print(FP)
+print(FN)
